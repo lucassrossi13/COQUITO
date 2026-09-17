a5e0e5876e091e273edfd65cc370b7000765d13c
lucassrossi13 <lucassrossi13@gmail.com>
2026-09-17 00:19:50 +0000

    Menu opcion 1

--- TP3-AED/clase.py
+++ TP3-AED/clase.py
@@ -7,27 +7,35 @@ class Tratamiento:
         self.nombre = nombre
         self.apellido = apellido
         self.codigo = codigo
-        self.monto_base = monto_base
+        self.monto_base = float(monto_base)
         self.complejidad = complejidad
-        self.algoritmo = algoritmo
-        self.monto_final = self.calcular_final(monto_base, complejidad, codigo, algoritmo)
+        self.algoritmo = int(algoritmo)
+        self.monto_final = self.calcular_final(float(monto_base), complejidad, codigo, int(algoritmo))
             
     def __str__(self):
-        return f"DNI: {self.dni}, Nombre: {self.nombre}, Apellido: {self.apellido}, Código: {self.codigo}, Monto Base: {self.monto_base}, Complejidad: {self.complejidad}, Monto Final: {self.monto_final}"
+        return f"Algoritmo: {self.algoritmo} DNI: {self.dni}, Nombre: {self.nombre}, Apellido: {self.apellido}, Código: {self.codigo}, Monto Base: {self.monto_base}, Complejidad: {self.complejidad}, Monto Final: {self.monto_final}"
             
-            
+    def __repr__(self):
+        return f"Algoritmo: {self.algoritmo} DNI: {self.dni}, Nombre: {self.nombre}, Apellido: {self.apellido}, Código: {self.codigo}, Monto Base: {self.monto_base}, Complejidad: {self.complejidad}, Monto Final: {self.monto_final}"
+                   
     
     def calcular_final(self, monto_base, complejidad, codigo, algoritmo):
-        
-        porcentaje_extra = int(codigo[4:])
+        bloque_icd = float(codigo[1:])
+        porcentaje_extra = float(codigo[4:])
         letra = codigo[0]
+        suma_fija = 0
+        monto_extra = 0
         monto_final = monto_base
         
+        
         if algoritmo == 1:
-            if monto_base > 60000:
-                monto_final = 0
-            else:
-                pass
+            
+            if monto_base <= 60000:
+                porcentaje_extra = 0
+            elif monto_base > 60000:
+                if complejidad == "A" and letra != "U":
+                    suma_fija = monto_base / 2
+            
         elif algoritmo == 2:
             if "A" <= letra <= "P":
                 pass
@@ -38,11 +46,23 @@ class Tratamiento:
                     porcentaje_extra = 15
                     
         elif algoritmo == 3:
-            pass
-        else:
-            pass
+            
+            if complejidad == "A":
+                monto_extra = monto_base * 0.30
+            
+            if "A" < letra < "L":
+                monto_extra += 20000
+            elif "M" < letra < "P":
+                monto_extra += 15000 + 5000 * bloque_icd
+            else:
+                monto_extra += monto_base * 0.10
+
+            if monto_extra > 60000:
+                monto_extra = 60000
         
-        return monto_final
+        monto_final += (monto_base * porcentaje_extra / 100) + suma_fija + monto_extra
+        
+        return round(monto_final, 2)
         
     
     
--- TP3-AED/principal.py
+++ TP3-AED/principal.py
@@ -1,11 +1,71 @@
 import clase
 
-archivo = open("tratamientos.csv")
 
+def menu():
+    print ('1. Cargar tratamientos')
+    print ('2. Mostrar resultados')
+    print('Ingrese 0 para salir')
 
-print(archivo.read())
+def buscar_apellido(tratamientos):
+    contador = 0
+    for tratamiento in tratamientos:
+        if tratamiento.complejidad == 'A':
+            contador += 1
+            if contador == 5:
+                return tratamiento.apellido
+    return 'No hay suficientes tratamientos de alta complejidad.'
 
+def cargar_tratamientos():
 
+    archivo = open('tratamientos.csv','rt')
+    archivo_leible = archivo.readlines()
+    tratamientos = []
 
+    n = len(archivo_leible)
+    c_tratamientos = n - 1 
+    #tratamientos = c_tratamientos * [None]
 
+    for i in range(1, n): #Corroborar el rango (1, n)
+        
+        tupla_linea = tuple(archivo_leible[i].split(','))
+        dni = tupla_linea[0]
+        nombre = tupla_linea[1]
+        apellido = tupla_linea[2]
+        codigo = tupla_linea[3]
+        monto_base = tupla_linea[4]
+        complejidad = tupla_linea[5]
+        algoritmo = tupla_linea[6]
+            
+        tratamiento = clase.Tratamiento(dni, nombre, apellido, codigo, monto_base, complejidad, algoritmo)
+        tratamientos.append(tratamiento)
+    return tratamientos, c_tratamientos
+        
+        
 
+def principal():
+    tratamientos = []
+    op = -1
+    
+    while op != 0:
+        menu()
+        op = int(input("Ingrese el número de la opción deseada: "))
+        
+        if op == 1:
+            tratamientos, c_tratamientos = cargar_tratamientos()
+            print(f'r1.1: {c_tratamientos}')
+            print(f'r1.2: {buscar_apellido(tratamientos)}')
+        elif op == 2:
+            pass
+        
+            
+        
+            
+    
+    
+    
+
+    
+    
+
+if __name__ == '__main__':
+    principal()
